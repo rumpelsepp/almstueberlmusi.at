d7fad705eab1c418ff2d371f58864d0816edb4e2
Stefan Tatschner <stefan.tatschner@mailbox.org>
2026-04-20 21:06:20 +0200

    strip down script

--- scripts/update-events.py
+++ scripts/update-events.py
@@ -3,69 +3,16 @@
 # /// script
 # requires-python = ">= 3.14"
 # dependencies = [
-#     "jinja2",
 #     "openpyxl",
 #     "pydantic",
 # ]
 # ///
 
-import json
 from datetime import datetime, date
 from typing import Any, cast
 
 import openpyxl
 import pydantic
-
-TPL = """---
-title: Veranstaltungen
-description: Kommende Veranstaltungen der Almstüberl Musi
----
-
-<table id="event-table" class="table table-striped">
-    <thead>
-        <tr>
-            <th>Datum</th>
-            <th>Veranstaltung</th>
-            <th>Ort</th>
-        </tr>
-    </thead>
-    <tbody>
-    {% for event in events %}
-        <tr>
-            <td><time datetime="{{ event.date.strftime('%Y-%m-%d') }}">{{ event.date.strftime('%d.%m.%Y') }}</time></td>
-            <td>
-            {% if event.url %}
-                <i class="bi bi-box-arrow-up-right"></i>
-                <a href="{{ event.url }}" target="_blank" rel="noopener noreferrer">{{ event.details }}</a>
-            {% else %}
-                {{ event.details }}
-            {% endif %}
-            </td>
-            <td>{{ event.location }}</td>
-        </tr>
-    {% endfor %}
-    </tbody>
-</table>
-
-<script>
-    document.addEventListener("DOMContentLoaded", function () {
-        const today = new Date();
-        today.setHours(0, 0, 0, 0); // Only date comparison, ignore time.
-
-        const rows = document.querySelectorAll("table#event-table tbody tr");
-
-        rows.forEach(row => {
-            const timeElement = row.querySelector("time");
-            if (timeElement) {
-                const date = new Date(timeElement.getAttribute("datetime"));
-                if (date < today) {
-                    row.classList.add("dimmed", "text-decoration-line-through");
-                }
-            }
-        });
-    });
-</script>
-"""
 
 
 class Event(pydantic.BaseModel):
@@ -100,17 +47,6 @@ def read_sheet() -> EventList:
         events.append(event)
 
     return EventList(events=sorted(events, key=lambda x: x.date, reverse=True))
-    
-
-def render_template(variables: dict[str, Any]) -> str:
-    return (
-        jinja2.Environment(
-            trim_blocks=True,
-            autoescape=False,
-        )
-        .from_string(TPL)
-        .render(variables)
-    )
 
 
 def main() -> None:
